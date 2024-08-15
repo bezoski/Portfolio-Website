dee3a6fadf9d985f763f728148223e8207ab2597
bezoski <rafalbuczkowski2004@wp.pl>
2024-08-15 12:57:59 +0200

    add test data to project.js

--- src/assets/Data/projects.js
+++ src/assets/Data/projects.js
@@ -1,27 +1,34 @@
 import welcomehome from '../Images/Projects/welcomehome.png';
 import flowvue from '../Images/Projects/flowvue.png';
 
+//testowe
+import gta6 from '../Images/Projects/gta6.png';
+import toolapp from '../Images/Projects/toolapp.jpg';
+
 const Projects = [
   {
     id: 1,
-    projectName: 'Real Estate Website',
-    image: welcomehome,
+    projectName: 'GTA 6',
+    category: 'game',
+    image: gta6,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    githubLink: 'https://github.com/bezoski/react-real-estate-app',
+    demoLink: 'https://www.rockstargames.com/pl/VI',
   },
   {
     id: 2,
     projectName: 'Flowvue app',
-    image: flowvue,
+    category: 'ui/ux',
+    image: toolapp,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
-    githubLink: 'https://github.com/bezoski/Automatizasion',
-    demoLink: 'https://flowvue.vercel.app/',
+    demoLink:
+      'https://dribbble.com/shots/24018370-Blueheart-Healthcare-Mobile-App-UI-Design',
   },
   {
     id: 3,
     projectName: 'Real Estate Website',
+    category: 'web',
     image: welcomehome,
     description:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ',
@@ -29,6 +36,7 @@ const Projects = [
   },
   {
     id: 4,
+    category: 'web',
     projectName: 'Flowvue app',
     image: flowvue,
     description:

4bfd53157aa2237c7be29ab84ed90c1a14700fd5
bezoski <rafalbuczkowski2004@wp.pl>
2024-08-17 13:44:26 +0200

    scroll fix buttons in about

--- src/components/About/Button.jsx
+++ src/components/About/Button.jsx
@@ -1,4 +1,8 @@
 const Button = ({ text, fill }) => {
+  const handleLinkClick = () => {
+    window.scrollTo(0, 0);
+  };
+
   let btnStyles =
     'font-merriweather lg:font-karla text-body-m lg:text-display-xs rounded-md px-5 lg:px-8 py-1 hover:text-neutral-night hover:bg-primary-floral-white transition duration-150 ease-in';
 
@@ -8,7 +12,11 @@ const Button = ({ text, fill }) => {
     btnStyles +=
       ' border border-primary-flame text-primary-flame hover:border-primary-floral-white ml-2';
   }
-  return <button className={btnStyles}>{text}</button>;
+  return (
+    <button className={btnStyles} onClick={handleLinkClick}>
+      {text}
+    </button>
+  );
 };
 
 export default Button;
